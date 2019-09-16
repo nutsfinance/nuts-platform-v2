076da53de259d1d25e18ea460f8b8c8d37297a9a
Shengda <dingshengda@gmail.com>
2019-09-16 08:54:05 -0700

    Complete instrument v2

--- contracts/instruments/v2/InstrumentV2.sol
+++ contracts/instruments/v2/InstrumentV2.sol
@@ -1,6 +1,7 @@
 pragma solidity ~0.5.0;
 
 import "../InstrumentBase.sol";
+import "../../escrow/EscrowBaseInterface.sol";
 import "../../storage/StorageInterface.sol";
 
 /**
@@ -8,90 +9,147 @@ import "../../storage/StorageInterface.sol";
  * A storage contract is created for each issuance.
  */
 contract InstrumentV2 is InstrumentBase {
-    /**
+       /**
      * @dev Create a new issuance of the financial instrument
      * @param issuanceId The id of the issuance
-     * @param issuanceStorage The storage contract of the issuance
-     * @param sellerAddress The address of the seller who creates this issuance
-     * @param sellerParameters The custom parameters to the newly created issuance
-     * @return transfers The transfers to perform after the invocation
+     * @param makerAddress The address of the maker who creates this issuance
+     * @param makerParameters The custom parameters to the newly created issuance
+     * @param issuanceStorage The storage contract for this issuance.
+     * @return updatedState The new state of the issuance.
      */
-    function createIssuance(uint256 issuanceId, StorageInterface issuanceStorage, address sellerAddress, bytes memory sellerParameters)
-        public returns (IssuanceStates updatedState, bytes memory transfers);
+    function createIssuance(uint256 issuanceId, address makerAddress, bytes memory makerParameters, StorageInterface issuanceStorage)
+        public returns (IssuanceStates updatedState);
 
     /**
-     * @dev A buyer engages to the issuance
+     * @dev A taker engages to the issuance
      * @param issuanceId The id of the issuance
+     * @param takerAddress The address of the taker who engages in the issuance
+     * @param takerParameters The custom parameters to the new engagement
+     * @param issuanceStorage The storage contract for this issuance.
      * @param state The current state of the issuance
-     * @param issuanceStorage The storage contract of the issuance
-     * @param balances The current balance of the issuance
-     * @param buyerAddress The address of the buyer who engages in the issuance
-     * @param buyerParameters The custom parameters to the new engagement
-     * @return transfers The transfers to perform after the invocation
+     * @param escrow The Issuance Escrow of the issuance
+     * @return updatedState The new state of the issuance.
+     * @return transfersData The transfers to perform after the invocation
      */
-    function engageIssuance(uint256 issuanceId, IssuanceStates state, StorageInterface issuanceStorage,
-        bytes memory balances, address buyerAddress, bytes memory buyerParameters)
-            public returns (IssuanceStates updatedState, bytes memory transfers);
+    function engageIssuance(uint256 issuanceId, address takerAddress, bytes memory takerParameters,
+        StorageInterface issuanceStorage, IssuanceStates state, EscrowBaseInterface escrow)
+        public returns (IssuanceStates updatedState, bytes memory transfersData);
 
     /**
-     * @dev The caller attempts to complete one settlement.
+     * @dev An account attempts to settle the issuance.
      * @param issuanceId The id of the issuance
-     * @param state The current state of the issuance
-     * @param issuanceStorage The storage contract of the issuance
-     * @param balances The current balance of the issuance
-     * @param settlerAddress The address of the buyer who settles in the issuance
+     * @param settlerAddress The address of the taker who settles the issuance.
      * @param settlerParameters The custom parameters to the settlement
-     * @return transfers The transfers to perform after the invocation
+     * @param issuanceStorage The storage contract for this issuance.
+     * @param state The current state of the issuance
+     * @param escrow The Issuance Escrow of the issuance
+     * @return updatedState The new state of the issuance.
+     * @return updatedData The updated data of the issuance.
+     * @return transfersData The transfers to perform after the invocation
      */
-    function settleIssuance(uint256 issuanceId, IssuanceStates state, StorageInterface issuanceStorage,
-        bytes memory balances, address settlerAddress, bytes memory settlerParameters)
-            public returns (IssuanceStates updatedState, bytes memory transfers);
+    function settleIssuance(uint256 issuanceId, address settlerAddress, bytes memory settlerParameters,
+        StorageInterface issuanceStorage, IssuanceStates state, EscrowBaseInterface escrow)
+        public returns (IssuanceStates updatedState, bytes memory transfersData);
 
     /**
-     * @dev Buyer/Seller has made an ERC20 token deposit to the issuance
+     * @dev An account has made an ETH deposit to the issuance
      * @param issuanceId The id of the issuance
+     * @param fromAddress The address of the ETH sender.
+     * @param amount The amount of ETH deposited.
+     * @param issuanceStorage The storage contract for this issuance.
      * @param state The current state of the issuance
-     * @param issuanceStorage The storage contract of the issuance
-     * @param balances The current balance of the issuance (after the deposit)
-     * @param fromAddress The address of the ERC20 token sender
-     * @param tokenAddress The address of the ERC20 token
-     * @param amount The amount of ERC20 token transfered
-     * @return updatedProperties The updated properties of the issuance.
-     * @return transfers The transfers to perform after the invocation
+     * @param escrow The Issuance Escrow of the issuance
+     * @return updatedState The new state of the issuance.
+     * @return updatedData The updated data of the issuance.
+     * @return transfersData The transfers to perform after the invocation
      */
-    function processTokenDeposit(uint256 issuanceId, IssuanceStates state, StorageInterface issuanceStorage,
-        bytes memory balances, address fromAddress, address tokenAddress, uint256 amount)
-            public returns (IssuanceStates updatedState, bytes memory transfers);
+    function processDeposit(uint256 issuanceId, address fromAddress, uint256 amount,
+        StorageInterface issuanceStorage, IssuanceStates state, EscrowBaseInterface escrow)
+        public returns (IssuanceStates updatedState, bytes memory transfersData);
 
     /**
-     * @dev Buyer/Seller has made an ERC20 token withdraw from the issuance
+     * @dev An account has made an ERC20 token deposit to the issuance
      * @param issuanceId The id of the issuance
+     * @param fromAddress The address of the ERC20 token sender.
+     * @param tokenAddress The address of the ERC20 token deposited.
+     * @param amount The amount of ERC20 token deposited.
+     * @param issuanceStorage The storage contract for this issuance.
      * @param state The current state of the issuance
-     * @param issuanceStorage The storage contract of the issuance
-     * @param balances The current balance of the issuance (after the withdraw)
-     * @param toAddress The address of the ERC20 token receiver
-     * @param tokenAddress The address of the ERC20 token
-     * @param amount The amount of ERC20 token transfered
-     * @return updatedProperties The updated properties of the issuance.
-     * @return transfers The transfers to perform after the invocation
+     * @param escrow The Issuance Escrow of the issuance
+     * @return updatedState The new state of the issuance.
+     * @return updatedData The updated data of the issuance.
+     * @return transfersData The transfers to perform after the invocation
      */
-    function processTokenWithdraw(uint256 issuanceId, IssuanceStates state, StorageInterface issuanceStorage,
-        bytes memory balances, address toAddress, address tokenAddress, uint256 amount)
-            public returns (IssuanceStates updatedState, bytes memory transfers);
+    function processTokenDeposit(uint256 issuanceId, address fromAddress, address tokenAddress, uint256 amount,
+        StorageInterface issuanceStorage, IssuanceStates state, EscrowBaseInterface escrow)
+        public returns (IssuanceStates updatedState, bytes memory transfersData);
+
 
     /**
-     * @dev Process customer event
+     * @dev An account has made an ETH withdraw from the issuance
      * @param issuanceId The id of the issuance
+     * @param toAddress The address of the ETH withdrawer.
+     * @param amount The amount of ETH withdrawn.
+     * @param issuanceStorage The storage contract for this issuance.
      * @param state The current state of the issuance
-     * @param issuanceStorage The storage contract of the issuance
-     * @param balances The current balance of the issuance
-     * @param notifierAddress The address which notifies this custom event
-     * @param eventName Name of the custom event, eventName of EventScheduled event
-     * @param eventPayload Payload of the custom event, eventPayload of EventScheduled event
-     * @return updatedProperties The updated properties of the issuance.
-     * @return transfers The transfers to perform after the invocation
+     * @param escrow The Issuance Escrow of the issuance
+     * @return updatedState The new state of the issuance.
+     * @return updatedData The updated data of the issuance.
+     * @return transfersData The transfers to perform after the invocation
      */
-    function processEvent(uint256 issuanceId, IssuanceStates state, StorageInterface issuanceStorage,
-        bytes memory balances, address notifierAddress, string memory eventName, bytes memory eventPayload)
-            public returns (IssuanceStates updatedState, bytes memory transfers);
+    function processWithdraw(uint256 issuanceId, address toAddress, uint256 amount,
+        StorageInterface issuanceStorage, IssuanceStates state, EscrowBaseInterface escrow)
+        public returns (IssuanceStates updatedState, bytes memory transfersData);
+
+    /**
+     * @dev An account has made an ERC20 token withdraw from the issuance
+     * @param issuanceId The id of the issuance
+     * @param toAddress The address of the ERC20 token withdrawer.
+     * @param tokenAddress The address of the ERC20 token withdrawn.
+     * @param amount The amount of ERC20 token withdrawn.
+     * @param issuanceStorage The storage contract for this issuance.
+     * @param state The current state of the issuance
+     * @param escrow The Issuance Escrow of the issuance
+     * @return updatedState The new state of the issuance.
+     * @return updatedData The updated data of the issuance.
+     * @return transfersData The transfers to perform after the invocation
+     */
+    function processTokenWithdraw(uint256 issuanceId, address toAddress, address tokenAddress, uint256 amount,
+        StorageInterface issuanceStorage, IssuanceStates state, EscrowBaseInterface escrow)
+        public returns (IssuanceStates updatedState, bytes memory transfersData);
+
+    /**
+     * @dev A custom event is triggered.
+     * @param issuanceId The id of the issuance
+     * @param notifierAddress The address of the caller who notifies the custom event.
+     * @param eventName The name of the custom event.
+     * @param eventPayload The custom parameters to the custom event
+     * @param issuanceStorage The storage contract for this issuance.
+     * @param state The current state of the issuance
+     * @param escrow The Issuance Escrow of the issuance
+     * @return updatedState The new state of the issuance.
+     * @return updatedData The updated data of the issuance.
+     * @return transfersData The transfers to perform after the invocation
+     */
+    function processCustomEvent(uint256 issuanceId, address notifierAddress, string memory eventName, bytes memory eventPayload,
+        StorageInterface issuanceStorage, IssuanceStates state, EscrowBaseInterface escrow)
+        public returns (IssuanceStates updatedState, bytes memory transfersData);
+
+    /**
+     * @dev A scheduled event is triggered.
+     * @param issuanceId The id of the issuance
+     * @param notifierAddress The address of the caller who notifies the scheduled event.
+     * @param eventName The name of the custom event.
+     * @param eventPayload The custom parameters to the custom event
+     * @param issuanceStorage The storage contract for this issuance.
+     * @param state The current state of the issuance
+     * @param escrow The Issuance Escrow of the issuance
+     * @return updatedState The new state of the issuance.
+     * @return updatedData The updated data of the issuance.
+     * @return transfersData The transfers to perform after the invocation
+     */
+    function processScheduledEvent(uint256 issuanceId, address notifierAddress, string memory eventName, bytes memory eventPayload,
+        StorageInterface issuanceStorage, IssuanceStates state, EscrowBaseInterface escrow)
+        public returns (IssuanceStates updatedState, bytes memory transfersData);
+
 }
--- contracts/instruments/v2/InstrumentV2Manager.sol
+++ contracts/instruments/v2/InstrumentV2Manager.sol
@@ -1,5 +1,230 @@
 pragma solidity ^0.5.0;
 
-contract InstrumentV2Manager {
-    
+import "../../access/WriterRole.sol";
+import "../../escrow/EscrowBaseInterface.sol";
+import "../../storage/StorageInterface.sol";
+import "../../storage/UnifiedStorage.sol";
+import "../InstrumentBase.sol";
+import "../InstrumentManagerBase.sol";
+import "./InstrumentV2.sol";
+
+contract InstrumentV2Manager is InstrumentManagerBase {
+
+    // Mapping: Issuance Id => Issuance storage contract address
+    mapping(uint256 => address) private _issuanceStorages;
+
+    /**
+     * @dev Instrument type-specific issuance creation processing.
+     * @param issuanceId ID of the issuance.
+     * @param makerAddress Address of the maker which creates the new issuance.
+     * @param makerParameters Custom issuance parameters.
+     */
+    function _processCreateIssuance(uint256 issuanceId, address makerAddress, bytes memory makerParameters)
+        internal returns (InstrumentBase.IssuanceStates updatedState) {
+
+        // Create storage contract
+        UnifiedStorage issuanceStorage = new UnifiedStorage();
+        _issuanceStorages[issuanceId] = address(issuanceStorage);
+
+        // Temporary grant writer role
+        issuanceStorage.addWriter(_instrumentAddress);
+
+        updatedState = InstrumentV2(_instrumentAddress).createIssuance(issuanceId, makerAddress, makerParameters,
+            issuanceStorage);
+
+        // Revoke writer role
+        issuanceStorage.removeWriter(_instrumentAddress);
+    }
+
+    /**
+     * @dev Instrument type-specific issuance engage processing.
+     * @param issuanceId ID of the issuance.
+     * @param takerAddress Address of the taker which engages the issuance.
+     * @param takerParameters Custom engagement parameters.
+     * @param state The current issuance state
+     * @param escrow The Issuance Escrow for this issuance.
+     */
+    function _processEngageIssuance(uint256 issuanceId, address takerAddress, bytes memory takerParameters,
+        InstrumentBase.IssuanceStates state, EscrowBaseInterface escrow)
+        internal returns (InstrumentBase.IssuanceStates updatedState, bytes memory transfersData) {
+
+        // Temporary grant writer role
+        UnifiedStorage issuanceStorage = UnifiedStorage(_issuanceStorages[issuanceId]);
+        issuanceStorage.addWriter(_instrumentAddress);
+
+        (updatedState, transfersData) = InstrumentV2(_instrumentAddress).engageIssuance(issuanceId,
+            takerAddress, takerParameters, issuanceStorage, state, escrow);
+
+        // Revoke writer role
+        issuanceStorage.removeWriter(_instrumentAddress);
+    }
+
+    /**
+     * @dev Instrument type-specific issuance settle processing.
+     * @param issuanceId ID of the issuance.
+     * @param settlerAddress Address of the caller who triggers settlement.
+     * @param settlerParameters Custom settlement parameters.
+     * @param state The current issuance state
+     * @param escrow The Issuance Escrow for this issuance.
+     */
+    function _processSettleIssuance(uint256 issuanceId, address settlerAddress, bytes memory settlerParameters,
+        InstrumentBase.IssuanceStates state, EscrowBaseInterface escrow)
+        internal returns (InstrumentBase.IssuanceStates updatedState, bytes memory transfersData) {
+
+        // Temporary grant writer role
+        UnifiedStorage issuanceStorage = UnifiedStorage(_issuanceStorages[issuanceId]);
+        issuanceStorage.addWriter(_instrumentAddress);
+
+        (updatedState, transfersData) = InstrumentV2(_instrumentAddress).settleIssuance(issuanceId,
+            settlerAddress, settlerParameters, issuanceStorage, state, escrow);
+        
+        // Revoke writer role
+        issuanceStorage.removeWriter(_instrumentAddress);
+    }
+
+    /**
+     * @dev Instrument type-specific issuance ETH deposit processing.
+     * Note: This method is called after deposit is complete, so that the Escrow reflects the balance after deposit.
+     * @param issuanceId ID of the issuance.
+     * @param fromAddress Address whose balance is the ETH transferred from.
+     * @param amount Amount of ETH deposited.
+     * @param state The current issuance state.
+     * @param escrow The Issuance Escrow for this issuance.
+     */
+    function _processDeposit(uint256 issuanceId, address fromAddress, uint256 amount,
+        InstrumentBase.IssuanceStates state, EscrowBaseInterface escrow)
+        internal returns (InstrumentBase.IssuanceStates updatedState, bytes memory transfersData) {
+
+        // Temporary grant writer role
+        UnifiedStorage issuanceStorage = UnifiedStorage(_issuanceStorages[issuanceId]);
+        issuanceStorage.addWriter(_instrumentAddress);
+
+        (updatedState, transfersData) = InstrumentV2(_instrumentAddress).processDeposit(issuanceId,
+            fromAddress, amount, issuanceStorage, state, escrow);
+
+        // Revoke writer role
+        issuanceStorage.removeWriter(_instrumentAddress);
+    }
+
+    /**
+     * @dev Instrument type-specific issuance ERC20 token deposit processing.
+     * Note: This method is called after deposit is complete, so that the Escrow reflects the balance after deposit.
+     * @param issuanceId ID of the issuance.
+     * @param fromAddress Address whose balance is the ERC20 token transferred from.
+     * @param tokenAddress Address the deposited ERC20 token.
+     * @param amount Amount of ERC20 deposited.
+     * @param state The current issuance state.
+     * @param escrow The Issuance Escrow for this issuance.
+     */
+    function _processTokenDeposit(uint256 issuanceId, address fromAddress, address tokenAddress, uint256 amount,
+        InstrumentBase.IssuanceStates state, EscrowBaseInterface escrow)
+        internal returns (InstrumentBase.IssuanceStates updatedState, bytes memory transfersData) {
+
+        // Temporary grant writer role
+        UnifiedStorage issuanceStorage = UnifiedStorage(_issuanceStorages[issuanceId]);
+        issuanceStorage.addWriter(_instrumentAddress);
+
+        (updatedState, transfersData) = InstrumentV2(_instrumentAddress).processTokenDeposit(issuanceId,
+            fromAddress, tokenAddress, amount, issuanceStorage, state, escrow);
+
+        // Revoke writer role
+        issuanceStorage.removeWriter(_instrumentAddress);
+    }
+
+    /**
+     * @dev Instrument type-specific issuance ETH withdraw processing.
+     * Note: This method is called after withdraw is complete, so that the Escrow reflects the balance after withdraw.
+     * @param issuanceId ID of the issuance.
+     * @param toAddress Address whose balance is ETH transferred to.
+     * @param amount Amount of ETH transferred.
+     * @param state The current issuance state.
+     * @param escrow Issuance Escrow for this issuance.
+     */
+    function _processWithdraw(uint256 issuanceId, address toAddress, uint256 amount,
+        InstrumentBase.IssuanceStates state, EscrowBaseInterface escrow)
+        internal returns (InstrumentBase.IssuanceStates updatedState, bytes memory transfersData) {
+
+        // Temporary grant writer role
+        UnifiedStorage issuanceStorage = UnifiedStorage(_issuanceStorages[issuanceId]);
+        issuanceStorage.addWriter(_instrumentAddress);
+
+        (updatedState, transfersData) = InstrumentV2(_instrumentAddress).processWithdraw(issuanceId,
+            toAddress, amount, issuanceStorage, state, escrow);
+
+        // Revoke writer role
+        issuanceStorage.removeWriter(_instrumentAddress);
+    }
+
+    /**
+     * @dev Instrument type-specific issuance ERC20 withdraw processing.
+     * Note: This method is called after withdraw is complete, so that the Escrow reflects the balance after withdraw.
+     * @param issuanceId ID of the issuance.
+     * @param toAddress Address whose balance is the ERC20 token transferred to.
+     * @param tokenAddress The ERC20 token to withdraw.
+     * @param amount Amount of ERC20 token to withdraw.
+     * @param state The current issuance state.
+     * @param escrow The Issuance Escrow for this issuance.
+     */
+    function _processTokenWithdraw(uint256 issuanceId, address toAddress, address tokenAddress, uint256 amount,
+        InstrumentBase.IssuanceStates state, EscrowBaseInterface escrow)
+        internal returns (InstrumentBase.IssuanceStates updatedState, bytes memory transfersData) {
+
+        // Temporary grant writer role
+        UnifiedStorage issuanceStorage = UnifiedStorage(_issuanceStorages[issuanceId]);
+        issuanceStorage.addWriter(_instrumentAddress);
+
+        (updatedState, transfersData) = InstrumentV2(_instrumentAddress).processTokenWithdraw(issuanceId,
+            toAddress, tokenAddress, amount, issuanceStorage, state, escrow);
+
+        // Revoke writer role
+        issuanceStorage.removeWriter(_instrumentAddress);
+    }
+
+    /**
+     * @dev Instrument type-specific custom event processing.
+     * @param issuanceId ID of the issuance.
+     * @param notifierAddress Address of the caller who notifies this custom event
+     * @param eventName Name of the custom event
+     * @param eventPayload Custom parameters for this custom event.
+     * @param state The current issuance state.
+     * @param escrow The Issuance Escrow for this issuance.
+     */
+    function _processCustomEvent(uint256 issuanceId, address notifierAddress, string memory eventName, bytes memory eventPayload,
+        InstrumentBase.IssuanceStates state, EscrowBaseInterface escrow)
+        internal returns (InstrumentBase.IssuanceStates updatedState, bytes memory transfersData) {
+
+        // Temporary grant writer role
+        UnifiedStorage issuanceStorage = UnifiedStorage(_issuanceStorages[issuanceId]);
+        issuanceStorage.addWriter(_instrumentAddress);
+
+        (updatedState, transfersData) = InstrumentV2(_instrumentAddress).processCustomEvent(issuanceId,
+            notifierAddress, eventName, eventPayload, issuanceStorage, state, escrow);
+
+        // Revoke writer role
+        issuanceStorage.removeWriter(_instrumentAddress);
+    }
+
+    /**
+     * @dev Instrument type-specific scheduled event processing.
+     * @param issuanceId ID of the issuance.
+     * @param notifierAddress Address of the caller who notifies this scheduled event.
+     * @param eventName Name of the schedule event
+     * @param eventPayload Custom parameters for this scheduled event
+     * @param state The current issuance state
+     * @param escrow The Issuance Escrow for this issuance
+     */
+    function _processScheduledEvent(uint256 issuanceId, address notifierAddress, string memory eventName, bytes memory eventPayload,
+        InstrumentBase.IssuanceStates state, EscrowBaseInterface escrow)
+        internal returns (InstrumentBase.IssuanceStates updatedState, bytes memory transfersData) {
+
+        // Temporary grant writer role
+        UnifiedStorage issuanceStorage = UnifiedStorage(_issuanceStorages[issuanceId]);
+        issuanceStorage.addWriter(_instrumentAddress);
+        
+        (updatedState, transfersData) = InstrumentV2(_instrumentAddress).processScheduledEvent(issuanceId,
+            notifierAddress, eventName, eventPayload, issuanceStorage, state, escrow);
+
+        // Revoke writer role
+        issuanceStorage.removeWriter(_instrumentAddress);
+    }
 }
